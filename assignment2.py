a722540905bce36fe5681c4e4625f5255c5c9714
neal.rogers@spsmail.cuny.edu <neal.rogers@spsmail.cuny.edu>
2017-02-07 18:08:39 -0500

    Minor edits to downloadData function

--- assignment2.py
+++ assignment2.py
@@ -4,11 +4,10 @@
 a file, and prints corresponding data for ID numbers.
 """
 
-import urllib2
+import urllib2, csv, datetime
 
 
 url = 'https://s3.amazonaws.com/cuny-is211-spring2015/birthdays100.csv'
-request = urllib2.Request(url)
 
 def downloadData(url):
     """
@@ -16,15 +15,17 @@ def downloadData(url):
         url (str): URL for fetching of data.
 
     Returns:
-        Contents of file.
+        data (): Contents of file.
 
     Example:
         >>> downloadData(url)
         >>>
     """
-    
+    request = urllib2.Request(url)
+    response = urllib2.urlopen(request)
+    data = response.read()
 
-# def processData(-):
+# def processData(data):
 #    """
 #    Args:
 #        url (str): URL for fetching of data.
