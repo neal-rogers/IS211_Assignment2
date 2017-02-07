36d2647c9a44c21f731b155b0fba1131e796f6e6
neal.rogers@spsmail.cuny.edu <neal.rogers@spsmail.cuny.edu>
2017-02-07 16:24:54 -0500

    Created and tested basic downloadData function

--- assignment2.py
+++ assignment2.py
@@ -1,6 +1,38 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""This program ***."""
+"""This program retrieves a CSV from a URL, processes it, logs invalid lines to
+a file, and prints corresponding data for ID numbers.
+"""
+
+import urllib2
 
 
+url = 'https://s3.amazonaws.com/cuny-is211-spring2015/birthdays100.csv'
+request = urllib2.Request(url)
 
+def downloadData(url):
+    """
+    Args:
+        url (str): URL for fetching of data.
+
+    Returns:
+        Contents of file.
+
+    Example:
+        >>> downloadData(url)
+        >>>
+    """
+    
+
+# def processData(-):
+#    """
+#    Args:
+#        url (str): URL for fetching of data.
+#
+#    Returns:
+#        Contents of file.
+#
+#    Example:
+#        >>> downloadData(url)
+#        >>>
+#    """
