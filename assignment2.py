747de1470507ce5b9fc1c5fe49253fc32f03f4a5
neal.rogers@spsmail.cuny.edu <neal.rogers@spsmail.cuny.edu>
2017-02-08 09:57:00 -0500

    Added processData method

--- assignment2.py
+++ assignment2.py
@@ -4,10 +4,8 @@
 a file, and prints corresponding data for ID numbers.
 """
 
-import urllib2, csv, datetime
+import urllib2, csv, datetime, logging
 
-
-url = 'https://s3.amazonaws.com/cuny-is211-spring2015/birthdays100.csv'
 
 def downloadData(url):
     """
@@ -15,25 +13,42 @@ def downloadData(url):
         url (str): URL for fetching of data.
 
     Returns:
-        data (): Contents of file.
+        data (): Contents of string.
 
     Example:
         >>> downloadData(url)
         >>>
     """
-    request = urllib2.Request(url)
-    response = urllib2.urlopen(request)
+    response = urllib2.urlopen(url)
     data = response.read()
+    return data
 
-# def processData(data):
-#    """
-#    Args:
-#        url (str): URL for fetching of data.
-#
-#    Returns:
-#        Contents of file.
-#
-#    Example:
-#        >>> downloadData(url)
-#        >>>
-#    """
+def processData(response_data):
+    """
+    Args:
+        url (str): URL for fetching of data.
+
+    Returns:
+        Contents of file.
+
+    Example:
+        >>> downloadData(url)
+        >>>
+    """
+    myresult_dict = {}
+    response_list = response_data.split("\n")
+
+    for rec_line in response_list:
+        rec = rec_line.split(",")
+        myresult_dict[rec[0]] = (rec[1], datetime.datetime.strptime(rec[2], "%d/%m/%Y"))
+
+    return myresult_dict
+
+if __name__ == "__main__":
+    url = "https://s3.amazonaws.com/cuny-is211-spring2015/birthdays100.csv"
+
+    birthdays_csv = downloadData(url)
+
+    results = processData(birthdays_csv)
+
+    print results
